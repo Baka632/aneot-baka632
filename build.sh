17f08a08a624e582e8be8984582837edadeea818
Baka632 <stevemc123456@outlook.com>
2024-09-22 15:36:45 +0800

    更新构建脚本

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-curl -sSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+curl -sSL https://deb.nodesource.com/setup_20.x > setup_nodejs_20.x.sh
+chmod +x setup_nodejs_20.x.sh
+./setup_nodejs_20.x.sh
 sudo apt-get install -y nodejs
 node -v
 npm install -g pnpm
