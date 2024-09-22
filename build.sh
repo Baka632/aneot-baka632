7acee5d4c6cdc47060068bc0af325b6b766f76b5
Baka632 <stevemc123456@outlook.com>
2024-09-22 16:11:33 +0800

    脚本现在会在构建前移除 node_modules 文件夹

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ echo "Installing Node Version Manager"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 20
 
 echo "Node and NPM version"
@@ -11,5 +10,6 @@ node -v
 npm -v
 
 echo "Preparing to build site..."
+rm -rf node_modules
 pnpm install
 pnpm run docs:build
