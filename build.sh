e2b39d80f223792697da416bb0d373023e9b9bf8
Baka632 <stevemc123456@outlook.com>
2024-09-22 16:01:47 +0800

    换用 nvm 来安装 node

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-brew install node@20
+echo "Installing Node Version Manager"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+nvm install 20
 
+echo "Node and NPM version"
+node -v
+npm -v
+
+echo "Preparing to build site..."
 pnpm install
 pnpm run docs:build
